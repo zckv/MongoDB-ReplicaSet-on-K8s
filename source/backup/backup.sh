a5e6c87f3a3f69bd7742ac977e5566fe18b59f6b
Panos Paparrigopoulos <panos.paparrigopoulos@cern.ch>
2022-06-27 15:27:12 +0200

    updates backup.sh

--- source/backup/backup.sh
+++ source/backup/backup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-source /etc/cmsweb-openrc.sh
 export OS_PROJECT_NAME="CMS Web"
+export OS_PROJECT_DOMAIN_ID="default"
+source /sec/cmsweb-openrc.sh
+openstack coe cluster list
 openstack volume snapshot create --volume $VOLUME_NAME $SNAPSHOT_NAME
