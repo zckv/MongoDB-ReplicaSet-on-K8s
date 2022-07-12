0fb555c8ac0efbf4304063f843464d75a47e8448
Panos Paparrigopoulos <panos.paparrigopoulos@cern.ch>
2022-07-12 13:24:33 +0200

    instroduction of monitoring changes and backup cron

--- source/backup/backup.sh
+++ source/backup/backup.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-export OS_PROJECT_NAME="CMS Web"
-export OS_PROJECT_DOMAIN_ID="default"
 source /sec/cmsweb-openrc.sh
 openstack coe cluster list
-openstack volume snapshot create --volume $VOLUME_NAME $SNAPSHOT_NAME
+openstack volume snapshot create --volume $VOLUME_NAME $SNAPSHOT_NAME --force 
