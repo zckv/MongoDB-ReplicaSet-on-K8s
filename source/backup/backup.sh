be48d39fdd23de8df2b1046d3490efd8c616aded
Panos Paparrigopoulos <panos.paparrigopoulos@cern.ch>
2022-07-18 11:23:15 +0200

    monitoring improvements

--- source/backup/backup.sh
+++ source/backup/backup.sh
@@ -2,4 +2,4 @@
 
 source /sec/cmsweb-openrc.sh
 openstack coe cluster list
-openstack volume snapshot create --volume $VOLUME_NAME $SNAPSHOT_NAME --force 
+openstack volume snapshot create --volume $VOLUME_NAME $SNAPSHOT_NAME --force  
